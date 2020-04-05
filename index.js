b7417ad695201e965a52a53f26d8eb4d04b53a32
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-05 11:14:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-
 function whisper(string) {
 return string.toLowerCase()
+}
+
+function logWhisper(string) {
+console.log(string.toUpperCase())
 }
