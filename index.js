6a9b0c52299f860f1e92b39929117f5b8ffe881c
Christoper Mercado <christoper_m15@yahoo.com>
2020-04-05 13:39:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,17 @@ function logWhisper(string) {
 string = string.toLowerCase()
 console.log(string)
 }
+
+function sayHiToGrandma(string) {
+
+if (string === "I love you, Grandma.") {
+return "I love you, too." }
+else if (string.toUpperCase() === string) {
+return "YES INDEED!" }
+else if (string.toLowerCase() === string) {
+return "I can't hear you!" }
+  
+}
+  
+}
+}
